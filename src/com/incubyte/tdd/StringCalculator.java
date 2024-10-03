bf730b7311bf104dc30d65151081b296ef6079da
AditiRDede <aditi.dede@gmail.com>
2024-10-04 01:01:34 +0530

    Project structure created along with  empty unit method

---
+++ src/com/incubyte/tdd/StringCalculator.java
@@ -0,0 +1,14 @@
+package com.incubyte.tdd;
+
+public class StringCalculator
+{
+	public static void main(String [] args)
+	{
+		System.out.println("String Calculator");
+	}
+	// Unit method
+	public int add(String numbers)
+	{
+		return 0;
+	}
+}
